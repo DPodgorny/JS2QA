73e1b436811eae6407069a56a33b87738bfdbca6
denis.podgorny <denispodgorny@gmail.com>
2018-05-08 17:19:15 +0300

    Updated Task1 (change Parameter and Values in object with each other) using another object

--- Task1.js
+++ Task1.js
@@ -1,47 +1,14 @@
-// Using Array
-
-var Object = {
+var TestObj = {
     parameter1 : 'value1',
     parameter2 : 'value2',
     parameter3 : 'value3'};
 
-var parameter = [], value = [];
-i=0;
+var TestObjNew = {};
 
-for (var key in Object)
+for (var key in TestObj)
 {
-    parameter[i] = key;
-    value[i] = Object[key];
-    i++;
+    var a=TestObj[key];
+    TestObjNew[a] = key;
 }
-
-i=0;
-
-for (var key in Object)
-{
-    delete Object[key];
-    key = value[i];
-    Object[key] = parameter[i];
-    i++
-}
-
-console.log(Object);
-
-// Without ARRAY
-var Object = {
-    parameter1 : 'value1',
-    parameter2 : 'value2',
-    parameter3 : 'value3'
-};
-
-for (var key in Object)
-{
-    a = key;
-    b = Object[key];
-    delete Object[key];
-    key = b;
-    Object[key] = a;
-}
-
-console.log(Object);
-
+TestObj=TestObjNew;
+console.log(TestObj);
