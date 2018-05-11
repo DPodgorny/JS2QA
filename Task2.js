f452099943b20badd558ead3e4a043d3bb8fdcac
denis.podgorny <denispodgorny@gmail.com>
2018-05-11 12:54:26 +0300

    Formatting for code for Task2

--- Task2.js
+++ Task2.js
@@ -1,22 +1,21 @@
-var Array = [3,2,3,1,4,5,5,6,2,2,4,2,3,2,4,1,4,3,4,5,4,6,4,7];
+var myArr = [3, 2, 3, 1, 4, 5, 5, 6, 2, 2, 4, 2, 3, 2, 4, 1, 4, 3, 4, 5, 4, 6, 4, 7, 1, 1, 3, 1, 4, 1, 5, 1, 6, 1, 8];
 
-var maxcount=0;
+var maxcount = 0;
 
-for (var b=0; b<Array.length; b++)
-{
-    var count=0;
-    for (var c=b; c<Array.length; c++)
-    {
-        if (Array[b]==Array[c])
-        {
+for (var b = 0; b < myArr.length; b++) {
+    var count = 0;
+
+    for (var c = b; c < myArr.length; c++) {
+
+        if (myArr[b] === myArr[c]) {
             count++;
         }
-        if (count>maxcount)
-        {
-            maxcount=count;
-            var number= b;
+
+        if (count > maxcount) {
+            maxcount = count;
+            var number = b;
         }
     }
 }
 
-console.log("The most often value is " + Array[number]+", repeats "+maxcount+" times");
+console.log("The most often value is " + myArr[number] + ", repeats " + maxcount + " times");
