e45077e1222cc03e8e7e66c37ee93f205e91c4ad
denis.podgorny <denispodgorny@gmail.com>
2018-05-11 17:40:45 +0300

    Created JS file with arrays with models for Website

---
+++ Website/modelList.js
@@ -0,0 +1,3 @@
+var modelListMerc = ['A-Class', 'B-Class', 'C-Class', 'E-Class', 'R-Class', 'S-Class', 'CLK', 'SLK'];
+var modelListToyota = ['Avensis', 'Camry', 'Corolla', 'Land Cruiser', 'Prius', 'Yaris'];
+var modelListLada = ['110', 'Largus', 'Vesta', 'X-Ray'];
