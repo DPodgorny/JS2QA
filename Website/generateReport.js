5d745d410e96225c0ee53006048bf7ae08eff11f
Denis <denispodgorny@gmail.com>
2018-05-14 00:06:04 +0300

    Table fill. (temp version, not finished)

--- Website/generateReport.js
+++ Website/generateReport.js
@@ -1,5 +1,37 @@
 function generateReport() {
-    var a=document.getElementById('Manufacturer').value;
-    var b=document.getElementById('Model').value;
-    console.log(a+b)
+    var man = document.getElementById('Manufacturer').value;
+    var mod = document.getElementById('Model').value;
+
+    var tableDiv = document.getElementById('tableDiv');
+    var table = document.createElement('table');
+    table.border = '3';
+    var tr = document.createElement('tr');
+    tr.border = '0';
+    var td = document.createElement('td');
+    td.border = '0';
+
+    tableDiv.appendChild(table);
+    table.appendChild(tr);
+    tr.appendChild(td);
+    td.appendChild(document.createTextNode(man));
+
+    if (mod != undefined)
+    {
+        var tr = document.createElement('tr');
+        table.append(tr);
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.appendChild(document.createTextNode(mod));
+        var td = document.createElement('td');
+        td.width = '100';
+        td.appendChild(document.createTextNode(''));
+    }
+    /*for (var a = 0; a<model.length; a++)
+    {
+        table.appendChild(tr);
+        td.appendChild(document.createTextNode(model));
+        tr.appendChild(td);
+        td.appendChild(document.createTextNode(man));
+        tr.appendChild(td);
+    }*/
 }
