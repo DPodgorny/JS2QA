2329c0fa6a62806a63403df4d85757f604da7736
Denis <denispodgorny@gmail.com>
2018-05-19 19:08:20 +0300

    API returns model list depending on received manufacturer

--- Website/Backend/websiteApi.js
+++ Website/Backend/websiteApi.js
@@ -1,4 +1,6 @@
-var manufacturerList = ['Mercedes', 'Toyota', 'Lada'];
+var modelListMerc = ['E-Class', 'S-Class', 'A-Class', 'C-Class', 'B-Class', 'R-Class', 'CLK', 'SLK'];
+var modelListToyota = [ 'Corolla',  'Yaris', 'Prius', 'Avensis', 'Camry', 'Land Cruiser'];
+var modelListLada = ['110', 'Vesta', 'Largus', 'X-Ray'];
 
 var http = require('http');
 
@@ -6,17 +8,34 @@ http.createServer(function(req, res) {
 
     var body = [];
 
-    req.on('data', function(chunk) {
-        console.log(chunk);
-        body.push(chunk);
+    req.on('data', function(requestMan) {
+        body.push(requestMan);
     }).on('end', function() {
+
         body = Buffer.concat(body).toString();
-        var a = JSON.parse(body);
-        console.log(a.name[0]);
+        //var man = JSON.parse(body);
+        var man = body;
 
+        switch (man) {
+
+            case 'Mercedes':
+                var modelList = modelListMerc;
+                break;
+            case 'Toyota':
+                var modelList = modelListToyota;
+                break;
+            case 'Lada':
+                var modelList = modelListLada;
+                break;
+            default:
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('Manufacturer is not valid');
+                return;
+        }
+
+        modelList = JSON.stringify(modelList);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(modelList);
     });
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('asdas');
 
 }).listen(8080);
