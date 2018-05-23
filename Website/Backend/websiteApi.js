2ecee16f8d198db19f421d4339e861a544598474
denis.podgorny <denispodgorny@gmail.com>
2018-05-23 18:06:14 +0300

    Created Backend API which returns list of models and API which returns models and status

--- Website/Backend/websiteApi.js
+++ Website/Backend/websiteApi.js
@@ -3,39 +3,106 @@ var modelListToyota = [ 'Corolla',  'Yaris', 'Prius', 'Avensis', 'Camry', 'Land 
 var modelListLada = ['110', 'Vesta', 'Largus', 'X-Ray'];
 
 var http = require('http');
+var url = require('url');
 
 http.createServer(function(req, res) {
 
-    var body = [];
+    var urlParse = url.parse(req.url).pathname;
+    var pathSplit = urlParse.split('/');
 
-    req.on('data', function(requestMan) {
-        body.push(requestMan);
-    }).on('end', function() {
+    if (pathSplit[1] === 'models') {
 
-        body = Buffer.concat(body).toString();
-        //var man = JSON.parse(body);
-        var man = body;
-
-        switch (man) {
-
+        switch (pathSplit[2]) {
             case 'Mercedes':
-                var modelList = modelListMerc;
+                var models = modelListMerc;
+                var responseObj = {'models': models};
                 break;
+
             case 'Toyota':
-                var modelList = modelListToyota;
+                var models = modelListToyota;
+                var responseObj = {'models': models};
                 break;
+
             case 'Lada':
-                var modelList = modelListLada;
+                var models = modelListLada;
+                var responseObj = {'models': models};
                 break;
+
+            default:
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('Manufacturer is not valid');
+                return;
+        }
+    }
+
+    else if (pathSplit[1] === 'status') {
+
+        console.log(pathSplit[2]);
+        switch (pathSplit[2]) {
+            case 'Mercedes':
+                var models = modelListMerc;
+                var responseObj = {'models': models};
+                break;
+
+            case 'Toyota':
+                var models = modelListToyota;
+                var responseObj = {'models': models};
+                break;
+
+            case 'Lada':
+                var models = modelListLada;
+                var responseObj = {'models': models};
+                break;
+
             default:
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end('Manufacturer is not valid');
                 return;
         }
 
-        modelList = JSON.stringify(modelList);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(modelList);
-    });
+        console.log(models);
+        if (pathSplit[3] === undefined)
+        {
+            var responseObj = {};
+            for (var a=0; a<models.length; a++) {
 
+                var modNameLength = models[a].length % 2;
+
+                if (modNameLength === 0) {
+                    var modelStatus = 'Not Available';
+                }
+                else {
+                    var modelStatus = 'Available';
+                }
+
+                responseObj[models[a]] = modelStatus;
+                console.log(responseObj);
+            }
+        }
+        else
+        {
+            var modNameLength = pathSplit[3].length % 2;
+
+            if (modNameLength === 0) {
+                var modelStatus = 'Not Available';
+            }
+            else {
+                var modelStatus = 'Available';
+            }
+            var responseObj = {};
+            responseObj[pathSplit[3]] = modelStatus;
+        }
+
+    }
+    else
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Request is not valid');
+        return;
+    }
+
+        var responseStr = JSON.stringify(responseObj);
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(responseStr);
 }).listen(8080);
